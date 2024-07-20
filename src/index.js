378561d3d78730ba5758bb65c09965360c12c591
Regina Ho <uhhvng@umkc.edu>
2024-07-20 18:42:22 -0500

    skill card created using React/Javascript

--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skills = [
+    {
+      skill: "HTML+CSS",
+      level: "intermediate",
+      color: "#2662EA"
+    },
+    {
+      skill: "JavaScript + REACT",
+      level: "intermediate",
+      color: "#EFD81D"
+    },
+    {
+      skill: "Web Design",
+      level: "intermediate",
+      color: "#C3DCAF"
+    },
+    {
+      skill: "Python",
+      level: "advanced",
+      color: "#E84F33"
+    },
+    {
+      skill: "Java",
+      level: "beginner",
+      color: "#60DAFB"
+    },
+    {
+      skill: "C++",
+      level: "intermediate",
+      color: "#FF3B00"
+    }
+  ];
+
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="ReginaH.jpg" alt="Regina Uyen Ho" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Regina Uyen Ho</h1>
+      <p>
+      A senior student at University of Missouri-Kansas City, majoring in Computer Science with a minor in Mathematics. 
+      I have a strong passion for web design and have honed my skills in both front-end and back-end programming.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+function Skill({skill,color,level}) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+      {level === "beginner" && "🐣"}
+      {level === "intermediate" && "✌️"}
+      {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
